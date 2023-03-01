c9d5ba5216861042ad1f9a03e1b2838416969cf4
EduardoMVAz <emendesvaz@gmail.com>
2023-03-01 17:29:31 -0300

    setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Linear_Algebra_Enigma_Encrypter
+
+Developers:
+
+* João Lucas de Moraes Barros Cadorniga [JoaoLucasMBC](https://github.com/JoaoLucasMBC)  
+* Eduardo Mendes Vaz [EduardoMVaz](https://github.com/EduardoMVAz)
---
+++ enigma_encrypter.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class EnigmaEncrypter:  
+
+    def __init__(self):
+        pass
+
+    def to_one_hot(self, message):
+        pass
+
+    def to_string(self, one_hot_message):
+        pass
+
+    def encrypt(self, message, P):
+        pass
+
+    def decrypt(self, message, P):
+        pass
+
+    def enigma(self, message, P, E):
+        pass
+
+    def de_enigma(self, message, P, E):
+        pass
