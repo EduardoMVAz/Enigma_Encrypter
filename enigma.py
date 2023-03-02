8643ec8ebc19dfa639903747f3f065c3fed6cff6
EduardoMVAz <emendesvaz@gmail.com>
2023-03-02 19:45:26 -0300

    to_one_hot

--- enigma.py
+++ enigma.py
@@ -2,11 +2,24 @@ import numpy as np
 
 class EnigmaEncrypter:  
 
+    ALFABETO = "abcdefghijklmnopqrstuvwxyz "
+
     def __init__(self):
         pass
 
     def to_one_hot(self, message):
-        pass
+        one_hot_message = np.empty((27, 0))
+        for letter in message.lower():
+            matriz = [[0] for i in range(27)]
+
+            i = self.ALFABETO.find(letter)
+
+            matriz[i] = [1]
+
+            one_hot_message = np.concatenate((one_hot_message, matriz), axis=1)
+
+        one_hot_message = np.array(one_hot_message)
+        return one_hot_message
 
     def to_string(self, one_hot_message):
         pass
