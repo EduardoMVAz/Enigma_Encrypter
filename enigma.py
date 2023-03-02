c660b82a5c5a621cbeeb26d8ba3a8bb97a97eade
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-03-02 16:45:52 -0300

    init

--- enigma.py
+++ enigma.py
@@ -1,15 +1,29 @@
 import numpy as np
+import random
+import copy
 
-class EnigmaEncrypter:  
+class Enigma:  
 
-    def __init__(self):
-        pass
+    def __init__(self, seed=42):
+        
+        self._seed = seed
+        random.seed(seed)
+
+        self.P = np.eye(27)
+        random.shuffle(self.P)
+
+        self.C = copy.deepcopy(self.P)
+        random.shuffle(self.C)
 
     def to_one_hot(self, message):
         pass
 
     def to_string(self, one_hot_message):
-        pass
+        message = ""
+
+        for col in one_hot_message.T:
+            pass
+
 
     def encrypt(self, message, P):
         pass
