6bd0c68642c0b3eb5fe32731d6d21444294803be
EduardoMVAz <emendesvaz@gmail.com>
2023-03-06 11:33:08 -0300

    library development

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='EnigmaEncrypter',
+    packages=find_packages(include=['enigma']),
+    version='0.1.0',
+    description='Enigma-like encrypter utilizing Linear Algebra',
+    author='João Lucas de Moraes Barros Cadorniga, Eduardo Mendes Vaz',
+    license='MIT'
+)
