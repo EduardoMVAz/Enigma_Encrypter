f29e557b10aff73fe496ee9d2a63eebb4bc53aeb
EduardoMVAz <emendesvaz@gmail.com>
2023-03-06 11:44:02 -0300

    putz

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='EnigmaEncrypter',
-    packages=find_packages(include=['enigma']),
+    packages=find_packages(include=['encrypter']),
     version='0.1.0',
     description='Enigma-like encrypter utilizing Linear Algebra',
     author='João Lucas de Moraes Barros Cadorniga, Eduardo Mendes Vaz',
